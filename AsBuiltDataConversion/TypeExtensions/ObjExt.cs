a1e6f38901843443189d80f327d6d27b216b77db
Stein Runar Bergheim <runar@bergheim.dk>
2015-08-22 19:50:46 +0200

    Added extension functions to C# object data type

---
+++ AsBuiltDataConversion/TypeExtensions/ObjExt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Norplan.Adm.AsBuiltDataConversion.TypeExtensions
+{
+    public static class ObjExt
+    {
+        public static int? AsIntegerOrNull(this object inputObject)
+        {
+            int? outValue = null;
+            int tmpValue;
+
+            if (int.TryParse(inputObject.ToString(), out tmpValue))
+            {
+                outValue = tmpValue;
+            }
+
+            return outValue;
+        }
+
+        /// <summary>
+        /// Converts an object to an integer if possible
+        /// </summary>
+        /// <param name="inputObject"></param>
+        /// <returns>Integer or exception</returns>
+        public static int AsInteger(this object inputObject)
+        {
+            try
+            {
+                return int.Parse(inputObject.ToString());
+            }
+            catch (Exception)
+            {
+                throw new Exception("Could not convert value " + inputObject.ToString() + " to an integer");
+            }
+        }
+
+    }
+}
