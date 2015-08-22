8b216f76ac90597820bafa9f8074ce15df7d132b
Stein Runar Bergheim <runar@bergheim.dk>
2015-08-22 20:42:35 +0200

    - Updated name space usage for partial class 'ExtFunctions'

--- AsBuiltDataConversion/Functions/ExtFunctions.CleanFileGDBNames.cs
+++ AsBuiltDataConversion/Functions/ExtFunctions.CleanFileGDBNames.cs
@@ -6,6 +6,7 @@ using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Norplan.Adm.AsBuiltDataConversion.TypeExtensions;
 
 namespace Norplan.Adm.AsBuiltDataConversion.Functions
 {
